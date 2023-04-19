65ca6be813f0474db527ae97370d6a640f8a2a43
Vladimir V. Andreev <zato66@gmail.com>
2023-04-19 18:33:49 +0300

    Update lesson_3.py

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -1,17 +1,38 @@
+import os
+from pympler import asizeof
 
-def find_in_file(file_name: str, pattern: str) -> list[str]:
-    results: list[any] = []
-    with open(file_name, "r") as file:
-        while word := file.readline():
-            if pattern in word.lower():
-                results.append(word)
-    return results
-    
+## Works but eats a lot of memory
+# def find_in_file(file_name: str, pattern: str) -> list[str]:
+#     results: list[any] = []
+#     with open(file_name, "r") as file:
+#         while word := file.readline():
+#             if pattern in word.lower():
+#                 results.append(word)
+#     return results
 
-pattern: str = input("Enter word: ")
+def find_in_file(file_name: str, pattern: str):
+    with open(file_name) as file:
+        while True:
+            line: str = file.readline()
+            if not line:
+                break
+            if pattern in line:
+                yield line
+
+pattern: str = input("Enter a pattern to search: ")
 file_name = "./rockyou.txt"
-results: list[str] = find_in_file(file_name, pattern)
-print(len(results))
+results_file = "./results.txt"
 
-with open("./results.txt", "w") as file:
+data = find_in_file(file_name, pattern)
+results = list(data)
+
+print(f'\nPattern {pattern} was found {len(results)} in words\n')
+print(f't\The first words are: {results[:10]}\n')
+
+with open(results_file, "w") as file:
     file.writelines(results)
+
+print(f'The size of results is: {asizeof.asizeof(results, stats = 0)}\n')
+
+print(os.stat(results_file))
+
