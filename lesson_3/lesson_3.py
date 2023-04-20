be9b1cf085563f0de413cc6effe681fe9d536f65
Vladimir V. Andreev <zato66@gmail.com>
2023-04-20 18:45:28 +0300

    Update lesson_3.py

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -13,26 +13,26 @@ from pympler import asizeof
 
 def find_in_file(file_name: str, pattern: str):
     """This function return line if find pattern."""
-    with open(file_name, encoding="utf-8") as file:
+    with open(file_name, encoding="utf-8") as ifile:
         while True:
-            line: str = file.readline()
+            line: str = ifile.readline()
             if not line:
                 break
             if pattern in line:
                 yield line
 
-pattern: str = input("Enter a pattern to search: ")
+my_pattern: str = input("Enter a pattern to search: ")
 FILE_NAME = "./rockyou.txt"
 RESULTS_FILE = "./results.txt"
 
-data = find_in_file(FILE_NAME, pattern)
+data = find_in_file(FILE_NAME, my_pattern)
 results = list(data)
 
-print(f'\nPattern {pattern} was found {len(results)} in words\n')
+print(f'\nPattern {my_pattern} was found {len(results)} in words\n')
 print(f'\nThe first words are: {results[:10]}\n')
 
-with open(RESULTS_FILE, "w", encoding="utf-8") as file:
-    file.writelines(results)
+with open(RESULTS_FILE, "w", encoding="utf-8") as rfile:
+    rfile.writelines(results)
 
 print(f'The size of results is: {asizeof.asizeof(results, stats = 0)}\n')
 
